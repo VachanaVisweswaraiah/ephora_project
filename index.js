3fce9c83be7a0417f7faceb63c131ac32cd80089
vachana333 <vachana.visweswaraiah3@gmail.com>
2025-07-10 21:43:28 +0200

    fix: removed static frontend serve for separate deployment

--- index.js
+++ index.js
@@ -8,37 +8,28 @@ import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
 import storyRoutes from './routes/story.route.js';
-import path from "path";
- 
-dotenv.config();
 
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const __dirname = path.resolve();
-
-//middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
+
 const corsOptions = {
     origin: process.env.URL,
     credentials: true
-}
+};
 app.use(cors(corsOptions));
 
-// yha pr apni api ayengi
+// Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
-app.use('/api/v1/story', storyRoutes);
+app.use("/api/v1/story", storyRoutes);
 
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-})
-
+// No static frontend serving here
 
 server.listen(PORT, () => {
     connectDB();
